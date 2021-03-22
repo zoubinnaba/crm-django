db68069d9ccc9d4448a509f094f887daf509109c
zoubinnaba <emmanuelzoubinnaba@gmail.com>
2021-03-22 19:17:44 +0000

    create leads app and configure on settings

--- crm/settings.py
+++ crm/settings.py
@@ -47,6 +47,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'leads',
 ]
 
 ROOT_URLCONF = 'crm.urls'
