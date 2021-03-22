b9d6a3dcd979b375c0f271aeaa87f92535511281
zoubinnaba <emmanuelzoubinnaba@gmail.com>
2021-03-22 19:42:40 +0000

    create lead model and migratiion

--- crm/settings.py
+++ crm/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'leads',
 ]
 
 MIDDLEWARE = [
@@ -47,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'leads',
 ]
 
 ROOT_URLCONF = 'crm.urls'
