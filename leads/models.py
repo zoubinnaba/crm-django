dd1f3201c1caa685b79c998eecc8368f8319e048
zoubinnaba <emmanuelzoubinnaba@gmail.com>
2021-03-23 08:41:21 +0000

    create agent model with relationship on lead model

--- leads/models.py
+++ leads/models.py
@@ -1,20 +1,27 @@
 from django.db import models
 
 
+
 class Lead(models.Model):
     
-    SOURCE_CHOICES = (
-        ("YouTube", "YouTube"),
-        ("Google", "Google"),
-        ("Newsletter", "Newsletter"),
-    )
+    # SOURCE_CHOICES = (
+    #     ("YouTube", "YouTube"),
+    #     ("Google", "Google"),
+    #     ("Newsletter", "Newsletter"),
+    # )
     
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
     
-    phoned = models.BooleanField(default=False)
-    source = models.CharField(max_length=100, choices=SOURCE_CHOICES)
+    # phoned = models.BooleanField(default=False)
+    # source = models.CharField(max_length=100, choices=SOURCE_CHOICES)
     
-    profile_picture = models.ImageField(blank=True, null=True)
-    special_fields = models.FileField(blank=True, null=True)
+    # profile_picture = models.ImageField(blank=True, null=True)
+    # special_fields = models.FileField(blank=True, null=True)
+    
+    
+    class Agent(models.Model):
+        first_name = models.CharField(max_length=20)
+        last_name = models.CharField(max_length=20)
