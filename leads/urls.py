458f36c59f7aec9f43ac45ae195fdaa543412496
zoubinnaba <emmanuelzoubinnaba@gmail.com>
2021-03-23 10:41:20 +0000

    create home views and templates

---
+++ leads/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from leads.views import (
+    home_page
+)
+
+app_name = "leads"
+
+urlpatterns = [
+    path('', home_page, name="home"),
+]
--- leads/views.py
+++ leads/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from leads.models import (
+    Lead,
+)
 
-# Create your views here.
+
+def home_page(request):
+    leads = Lead.objects.all()
+    return render(request, 'leads/home_page.html', {
+        "leads": leads
+    })
