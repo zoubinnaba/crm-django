ef98c9be12422ae677b634fcf250c622a09910a8
zoubinnaba <emmanuelzoubinnaba@gmail.com>
2021-03-24 12:36:59 +0000

    create a send mail for lead creation

--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect, reverse 
-from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
-
+from django.core.mail import send_mail
+from django.shortcuts import  reverse 
+from django.views import generic 
 from leads.models import (
     Lead,
     Agent,
@@ -9,35 +9,43 @@ from leads.models import (
 from leads.forms import LeadModelForm
 
 
-class LandingPageView(TemplateView):
+class LandingPageView(generic.TemplateView):
     template_name = "landing.html"
 
 
 
-class LeadListView(ListView):
+class LeadListView(generic.ListView):
     template_name = "leads/lead_list.html"
     queryset = Lead.objects.all()
     context_object_name = "leads"
 
 
-class LeadDetailView(DetailView):
+class LeadDetailView(generic.DetailView):
     template_name = "leads/lead_detail.html"
     queryset = Lead.objects.all()
     context_object_name = "lead"
 
 
 
-class LeadCreateView(CreateView):
+class LeadCreateView(generic.CreateView):
     template_name = "leads/lead_create.html"
     form_class = LeadModelForm
     
     def  get_success_url(self):
         return reverse("leads:lead_list")
         
+    def form_valid(self, form):
+        # TODO send mail
+        send_mail(
+            subject="A lead has been created",
+            message="Go to the site to see the new lead",
+            from_email="test@test.com",
+            recipient_list=["test2@test.com"]
+        )
+        return super(LeadCreateView, self).form_valid(form)
 
 
-
-class LeadUpdateView(UpdateView):
+class LeadUpdateView(generic.UpdateView):
     template_name = "leads/lead_update.html"
     queryset = Lead.objects.all()
     form_class = LeadModelForm
@@ -46,8 +54,8 @@ class LeadUpdateView(UpdateView):
         return reverse("leads:lead_list")
     
 
-class LeadDeleteView(DeleteView):
-    template_name = "leads/lead_update.html"
+class LeadDeleteView(generic.DeleteView):
+    template_name = "leads/lead_delete.html"
     queryset = Lead.objects.all()
     form_class = LeadModelForm
     
