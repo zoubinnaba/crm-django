973e9fd0e1d2f0104e77a7c1f1699a5aa97c8abf
zoubinnaba <emmanuelzoubinnaba@gmail.com>
2021-03-24 15:00:18 +0000

    start test in django

---
+++ leads/tests/tests_views.py
@@ -0,0 +1,11 @@
+from django.test import TestCase
+from django.shortcuts import  reverse
+
+
+
+class HomePageTest(TestCase):
+    
+    def  test_get(self):
+        response =  self.client.get(reverse("landing-page"))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "landing.html")
