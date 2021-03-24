e161fc28fefb5dc01bc0caf87579e6a3a5536f15
zoubinnaba <emmanuelzoubinnaba@gmail.com>
2021-03-24 16:20:29 +0000

    create signal for user creation

--- leads/models.py
+++ leads/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.signals import post_save
 from django.contrib.auth.models import AbstractUser
 
 
@@ -31,3 +32,11 @@ class Agent(models.Model):
     
     def __str__(self) -> str:
         return self.user.email
+    
+    
+def post_user_created_signal(sender, instance, created, **kwargs):
+    print(instance)
+    if created:
+        UserProfil.objects.create(user=instance)
+
+post_save.connect(post_user_created_signal, sender=User)
