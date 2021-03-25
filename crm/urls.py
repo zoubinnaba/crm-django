709d1530fbdd0154feb658947be15653ae6f516e
zoubinnaba <emmanuelzoubinnaba@gmail.com>
2021-03-25 21:03:07 +0000

    create the rest password views

--- crm/urls.py
+++ crm/urls.py
@@ -1,15 +1,26 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordResetView,
+    PasswordResetDoneView,
+    PasswordResetConfirmView,
+    PasswordResetCompleteView
+)
 from django.urls import path, include
-from leads.views import  LandingPageView, SignupView
+from leads.views import LandingPageView, SignupView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', LandingPageView.as_view(), name="landing-page"),
     path('leads/', include('leads.urls', namespace='leads')),
     path('signup/', SignupView.as_view(), name='signup'),
+    path('reset-password/', PasswordResetView.as_view(), name="rest-password"),
+    path('password-rest-done/', PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('password-rest-conform/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('password-rest-complete/', PasswordResetCompleteView.as_view(), name="password_reset_complete"),
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     # url for agents
@@ -17,4 +28,4 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
