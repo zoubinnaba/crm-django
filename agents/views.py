2843dd4e0c32e2ffcb27bb51cc0439e0dfadff69
zoubinnaba <emmanuelzoubinnaba@gmail.com>
2021-03-25 09:25:12 +0000

    filter user

--- agents/views.py
+++ agents/views.py
@@ -9,7 +9,8 @@ class AgentListView(LoginRequiredMixin, generic.ListView):
     template_name = "agents/agent_list.html"
     
     def get_queryset(self):
-        return Agent.objects.all()
+        organisation = self.request.user.userprofil
+        return Agent.objects.filter(organisation=organisation)
     
 
 class AgentCreateView(LoginRequiredMixin, generic.CreateView):
@@ -25,3 +26,35 @@ class AgentCreateView(LoginRequiredMixin, generic.CreateView):
         agent.save()
         return super(AgentCreateView, self).form_valid(form)
         
+        
+class AgentDetailView(LoginRequiredMixin, generic.DetailView):
+    template_name = "agents/agent_detail.html"
+    context_object_name = "agent"
+    
+    def get_queryset(self):
+        organisation = self.request.user.userprofil
+        return Agent.objects.filter(organisation=organisation)
+    
+
+class AgentUpdateView(LoginRequiredMixin, generic.UpdateView):
+    template_name = "agents/agent_update.html"
+    form_class = AgentModelForm
+    
+    def get_success_url(self):
+        return reverse("agents:agent-list")
+    
+    def get_queryset(self):
+        organisation = self.request.user.userprofil
+        return Agent.objects.filter(organisation=organisation)
+    
+
+class AgentDeleteView(LoginRequiredMixin, generic.DeleteView):
+    template_name = "agents/agent_delete.html"
+    context_object_name = "agent"
+    
+    def get_queryset(self):
+        organisation = self.request.user.userprofil
+        return Agent.objects.filter(organisation=organisation)
+    
+    def get_success_url(self):
+        return reverse("agents:agent-list")
