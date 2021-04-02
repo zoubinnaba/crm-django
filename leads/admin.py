d8e6e0dd41498c304444a1cf8a1a8c85fcf032a3
zoubinnaba <emmanuelzoubinnaba@gmail.com>
2021-04-02 13:30:46 +0000

    add the admin panel fields

--- leads/admin.py
+++ leads/admin.py
@@ -9,9 +9,18 @@ from leads.models import (
 )
 
 
+class LeadAdmin(admin.ModelAdmin):
+    # fields = (
+    #     'first_name',
+    #     'last_name'
+    # )
+    list_display = ['first_name', 'last_name', 'age', 'email']
+    list_filter = ['category']
+    search_fields = ['first_name', 'last_name']
+
+
 admin.site.register(User)
-admin.site.register(Lead)
+admin.site.register(Lead, LeadAdmin)
 admin.site.register(Agent)
 admin.site.register(UserProfile)
 admin.site.register(Category)
-
